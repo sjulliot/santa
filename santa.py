4e14ce4b87cbb5fc356bc0bddba1d0065ef877bf
Sebastien Julliot <sebastien@jdlm.tech>
2016-12-11 22:05:39 +0100

    fixing sender and receiver name in mail

--- santa.py
+++ santa.py
@@ -15,15 +15,16 @@ __email__ = 'julliot@ljll.math.upmc.fr'
 
 User = namedtuple('User', ['first', 'mail'])
 
-msg = ('Bonjour {user.first},\n'
+msg = ('Wesh {user.first},\n'
        '\n'
        'Santa t\'a désigné pour donner un cadeau à {give_to.first} cette année.\n'
        '\n'
-       'Joyeuses Paques !')
+       'Que le dauphin soit avec toi !')
 
-message_subject = '[SANTA] Pour la chaine de cadeaux tu devras donner le tien à ...'
+message_subject = '[SANTA] Pour la chaine de cadeaux tu devras donner le tien à ... #LienPutaclic'
 
 smtp_server = 'smtp.jdlm.tech'
+from_mail = 'no-reply@jdlm.tech'
 login = 'sebastien@jdlm.tech'
 passwd = ''
 
@@ -31,13 +32,14 @@ server = smtplib.SMTP(smtp_server)
 server.login(login, passwd)
 
 def send_mail(user, give_to, msg, subject):
+    print ('Envoi à {}'.format(give_to.first))
     msg_text = msg.format(user=user, give_to=give_to)
     msg = MIMEText(msg_text, 'plain', 'utf-8')
     msg['Subject'] = subject
-    msg['From'] = user.mail
-    msg['To'] = give_to.mail
+    msg['From'] = from_mail
+    msg['To'] = user.mail
 
-    server.sendmail(user.mail, [give_to.mail], msg.as_string())
+    server.sendmail(from_mail, [user.mail], msg.as_string())
 
 def permute(l):
     copy = list(l)
